757d41e2967c9509103d1fafc41d9e4ff3a685af
Jose Luis <joseluis@priego8a.com>
2020-04-28 19:54:15 -0500

    App eats agregada al proyecto.

---
+++ AppEats/scripts/auth.js
@@ -0,0 +1,105 @@
+
+auth.onAuthStateChanged( user => {
+    configurarMenu(user);
+});
+
+const formIngresar = document.getElementById("formIngresar");
+
+formIngresar.addEventListener("submit", (e) => {
+    e.preventDefault(); // evitar el reload
+
+    let correo = formIngresar["correo"].value;
+    let contrasena = formIngresar["contrasena"].value;
+
+    auth.signInWithEmailAndPassword(correo, contrasena).then( credentials => {
+        console.log("Login exitoso.");
+
+        $("#modalIngresar").modal("hide");
+        formIngresar.reset();
+        formIngresar.querySelector(".error").innerHTML = "";
+
+    }).catch( error => {
+        console.log("Error en login: ");
+        formIngresar.querySelector(".error").innerHTML = messageError(error.code);
+    });
+
+});
+
+function messageError(errCode) {
+    let mensaje = "";
+    console.log(errCode)
+    switch (errCode) {
+        case "auth/wrong-password":
+            mensaje = "Contraseña incorrecta";
+            break;
+        case "auth/user-not-found":
+            mensaje = "Usuario no encontrado";
+            break;
+        case "auth/weak-password":
+            mensaje = "La contraseña es débil";
+            break;
+        default:
+            mensaje = "Hay un error al ingresar con este usuario."
+    }
+
+    return mensaje;
+}
+
+const salir = document.getElementById("btnSalir");
+salir.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    auth.signOut().then( () => {
+        alert("Has salido de la sesión");
+        $("#collapseExample").collapse("hide");
+    });
+});
+
+const formRegistro = document.getElementById("formRegistro");
+formRegistro.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const correo = formRegistro["correo_registro"].value;
+    const contrasena = formRegistro["contrasena_registro"].value;
+
+    auth.createUserWithEmailAndPassword(correo, contrasena).then( credentials => {
+        
+        db.collection("usuarios").doc(credentials.user.uid).set({
+            nombre: formRegistro["nombre_registro"].value,
+            telefono: formRegistro["telefono_registro"].value,
+            direccion: formRegistro["direccion_registro"].value
+        });
+
+    }).then( () => {
+        
+        console.log("Registrado!");
+        $("#modalRegistro").modal("hide");
+        formRegistro.querySelector(".error").innerHTML = "";
+
+    }).catch( error => {
+        console.log("Error en registro: ");
+        formRegistro.querySelector(".error").innerHTML = messageError(error.code);
+        formRegistro.reset();
+    });
+
+});
+
+
+function loginGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then( function(res) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+
+        document.getElementById("menuNombre").innerHTML = res.user.displayName
+        document.getElementById("menuTelefono").innerHTML = res.user.email;
+        document.getElementById("menuDireccion").innerHTML = '';
+        $("#menuImg").attr("src",res.user.photoURL);
+
+        $("#modalIngresar").modal("hide");
+        formIngresar.reset();
+        formIngresar.querySelector(".error").innerHTML = "";
+
+    });
+}
---
+++ AppEats/scripts/index.js
@@ -0,0 +1,60 @@
+
+
+const listaToLog = document.querySelectorAll(".toLog");
+const listaLogged = document.querySelectorAll(".logged");
+
+const configurarMenu = (user) => {
+    if (user) {
+
+        db.collection("usuarios").doc(user.uid).get().then( doc => {
+            if (doc.data().nombre) {
+                $("#menuImg").attr("src","");
+                document.getElementById("menuNombre").innerHTML = doc.data().nombre;
+                document.getElementById("menuTelefono").innerHTML = "Teléfono: " + doc.data().telefono;
+                document.getElementById("menuDireccion").innerHTML = "Dirección: " + doc.data().direccion;
+            }
+        });
+
+        db.collection("platillos").onSnapshot( snap => {
+            ponerPlatillos(snap.docs);
+        });
+
+        listaLogged.forEach( i => i.style.display = "block");
+        listaToLog.forEach( i => i.style.display = "none");
+    } else {
+        listaLogged.forEach( i => i.style.display = "none");
+        listaToLog.forEach( i => i.style.display = "block");
+
+        document.getElementById("listaPlatillos").innerHTML = `
+        <div class="container mt-4 col-12 shadow" style="border-radius: 20px; background-color: yellowgreen; height: 140px; display: flex; justify-content: center; align-items: center;">
+                      <h2 style="color: white;">Inicia sesión para pedir eso que se te antoja</h2>
+                  </div>
+        `;
+    }
+};
+
+function ponerPlatillos(data) {
+    
+    if (data.length) {
+        var html = '';
+        data.forEach( p => {
+            const platillo = p.data();
+            const columna = `
+            <div class="card shadow mr-3 ml-3" style="width: 18rem; border-radius: 25px;">
+                    <img src="images/${platillo.imagen}" class="card-img-top" alt="Pizza">
+                    <div class="card-body">
+                        <h5>${platillo.nombre}</h5>
+                        <p class="card-text">Las mejores pizzas</p>
+                        <p class="card-text" style="text-align: end;">$${platillo.precio} MXN</p>        
+                    </div>
+                </div>
+            `;
+
+            html += columna;
+
+        });
+
+        document.getElementById("listaPlatillos").innerHTML = html;
+
+    }
+}
